27d4cf5c87b754df1ebbc4dcabed8a9eaf8e5f6d
Ansar Ismagilov <ansar89kab@bk.ru>
2022-12-28 23:16:52 +0400

    commit_7 - done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Zombie zombie = new Zombie("Bob");
-        zombie.attack();
+        Zombie zombie1 = new Zombie("Alice");
+        zombie1.attack();
+        zombie1.growl();
+        Zombie zombie2 = new Zombie("Bob");
+        zombie2.attack();
+        zombie2.growl();
     }
 }
